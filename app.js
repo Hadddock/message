60b4a94df6b710f542d068be3959c3a494d86a3c
Haddock <jhadddock@protonmail.com>
2023-12-19 15:09:31 -0800

    Add Mongoose

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.ConnectionString;
 
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
