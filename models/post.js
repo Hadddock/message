c6c487697e1d716a0fb9661720b5617c19608833
Haddock <jhadddock@protonmail.com>
2023-12-20 19:33:19 -0800

    Change content to message

--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
   dateCreated: { type: Date },
-  content: { type: String, required: true, maxLength: 60000 },
+  message: { type: String, required: true, maxLength: 60000 },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
