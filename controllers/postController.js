e08f23883979ed444712e9f4366d5ac4ccc87385
Haddock <jhadddock@protonmail.com>
2023-12-20 19:34:08 -0800

    Add postcontroller functions

---
+++ controllers/postController.js
@@ -0,0 +1,44 @@
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const Post = require("./../models/post");
+
+exports.create_post_post = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+};
+
+exports.create_post_get = (req, res, next) => {
+  res.render("createMessage");
+};
+
+exports.create_post_post = [
+  //validate and sanitize
+  body("message")
+    .trim()
+    .isLength({ min: 1, max: 60000 })
+    .withMessage("Post must be between 1-60000 characters")
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    //extract validation errors from request
+    const errors = validationResult(req);
+
+    //create new user object
+    const post = new Post({
+      user: req.user._id,
+      date_created: new Date(),
+      message: req.body.message,
+    });
+    //render form again with errors
+    if (!errors.isEmpty()) {
+      res.render("createMessage", { post: post, errors: errors.array() });
+      return;
+    } else {
+      await post.save();
+      res.redirect("/");
+    }
+  }),
+];
--- routes/catalog.js
+++ routes/catalog.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const user_controller = require("../controllers/userController");
+const post_controller = require("../controllers/postController");
 
 router.get("/sign-up", user_controller.sign_up_get);
 router.post("/sign-up", user_controller.sign_up_post);
@@ -10,5 +11,8 @@ router.get("/login", user_controller.login_get);
 router.post("/login", user_controller.login_post);
 
 router.post("/logout", user_controller.logout_post);
+
+router.get("/create-post", post_controller.create_post_get);
+router.post("/create-post", post_controller.create_post_post);
 
 module.exports = router;
